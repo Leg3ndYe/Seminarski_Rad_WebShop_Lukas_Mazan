0f8f176c3530c07673373faa1453dad24bd57f3e
Lukas Mazan <lukass122.lukas@gmail.com>
2023-10-15 10:53:25 +0200

    Configured carousel, added methods for orders and started working on order view

--- Motorcycle_WebShop/Controllers/HomeController.cs
+++ Motorcycle_WebShop/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Motorcycle_WebShop.Data;
+using Motorcycle_WebShop.Extensions;
 using Motorcycle_WebShop.Models;
 using System.Diagnostics;
 
@@ -9,7 +11,7 @@ namespace Motorcycle_WebShop.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly ApplicationDbContext _context;
-
+        public const string SessionKeyName = "_cart";
         public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
@@ -26,9 +28,17 @@ namespace Motorcycle_WebShop.Controllers
             return View();
         }
 
-        public IActionResult Product(int? categoryId)
+        public IActionResult Product(int? id, int? categoryId)
         {
-            List<Product> products = _context.Product.ToList();
+            List<Product> products = _context.Product.Where(x => x.IsActive == true).ToList();
+
+            if(id != null)
+            {
+                var product = products.Where(p => p.Id == id).FirstOrDefault();
+                product.ProductImages = _context.ProductImage.Where(pi => pi.ProductId == id).ToList();
+                product.ProductCategories = _context.ProductCategory.Where(pc => pc.ProductId == product.Id).ToList();
+                return View("ProductDetails", product);
+            }
 
             foreach (var product in products)
             {
@@ -43,6 +53,50 @@ namespace Motorcycle_WebShop.Controllers
 
             ViewBag.Categories = _context.Category.ToList();
             return View(products);
+        }
+
+        public IActionResult Order(List<string> errors)
+        {
+            List<CartItem> cart = HttpContext.Session.GetObjectFromJson<List<CartItem>>(SessionKeyName);
+            if (cart == null) return RedirectToAction("Index");
+            if (cart.Count == 0) return RedirectToAction("Index");
+
+            for(int i=0; i<cart.Count; i++)
+            {
+                var product = _context.Product.Find(cart[i].Product.Id);
+                if(product== null)
+                {
+                    cart.RemoveAt(i);
+                    i--;
+                    errors.Add("Product not found and was removed from cart.");
+                    continue;
+                }
+                if(product.Quantity < cart[i].Quantity)
+                {
+                    cart[i].Quantity = product.Quantity;
+                    errors.Add("Product quantity was reduced to available quantity!");
+                }
+                if(!product.IsActive)
+                {
+                    cart.RemoveAt(i);
+                    i--;
+                    errors.Add("Product is not active and was removed from cart!");
+                    continue;
+                }
+                
+            }
+
+            HttpContext.Session.SetObjectAsJson(SessionKeyName, cart);
+
+            foreach(CartItem item in cart)
+            {
+                item.Product.ProductImages=_context.ProductImage.Where(pi => pi.ProductId==item.Product.Id).ToList();
+                item.Product.ProductCategories = _context.ProductCategory.Where(pc => pc.ProductId == item.Product.Id).ToList();
+            }
+
+            ViewBag.Error = errors;
+
+            return View(cart);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
